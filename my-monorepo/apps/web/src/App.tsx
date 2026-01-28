00857e710eb3133dcd01e827482352590fd86116
Adam Zagórski <kontakt@adamzagorski.pl>
2026-01-28 16:03:09 +0100

    add placeholder for my feature page:

--- my-monorepo/apps/web/src/App.tsx
+++ my-monorepo/apps/web/src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>Jestem Adam</h1>
+      <h2>Aspiruję na stanowisko Junior Front-end developera</h2>
+
+      <p>
+        W tym miejscu planuję zebrać moje projekty i pomysły, aby prezentować
+        umiejętności i kompetnecje
+      </p>
+      <p>
+        Do tej pory zebrałem
+        <strong> 6 miesięcy komercyjnego doświadczenia</strong> na AGH
+      </p>
+      <p></p>
+
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        <a
+          href="https://github.com/adamzagorski92/turbo-adam"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Tutaj możesz obejrzeć kod źródłowy tej strony
+        </a>
+      </p>
+      <p className="read-the-docs">
+        <a
+          href="https://www.linkedin.com/in/adazag/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Tutaj mój Linkedin
+        </a>
+      </p>
+      <p>
+        <a
+          href="https://www.adamzagorski.pl/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Tutaj trochę więcej o mnie
+        </a>
+      </p>
+      <hr />
+      <p>
+        Stronę zbudowałem przy użyciu React, TypeScript i Turborepo na serwerze
+        deweloperskim <a href="https://mikr.us/?r=adamzagorski">Mikrus</a>.
+        Więcej informacji w kodzie źródłowym.
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
