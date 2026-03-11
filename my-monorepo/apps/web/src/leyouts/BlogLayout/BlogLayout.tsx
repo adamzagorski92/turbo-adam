904026816db46901b71ea9dc77a346ef6c296932
Adam Zagórski <kontakt@adamzagorski.pl>
2026-03-11 11:42:48 +0100

    [#55] refactor(layout): simplified BlogLayout component

--- my-monorepo/apps/web/src/leyouts/BlogLayout/BlogLayout.tsx
+++ my-monorepo/apps/web/src/leyouts/BlogLayout/BlogLayout.tsx
@@ -1,7 +1,11 @@
 import { Outlet } from "react-router";
 import { PageContainer } from "@packages/components/basePageContainers/PageContainer/PageContainer";
 import Footer from "@features/Footer/Footer";
-import { ColumnSection, InnerColumnSection } from "@packages/components";
+import {
+  ColumnSection,
+  InnerColumnSection,
+  PageBody,
+} from "@packages/components";
 import Logo from "@components/Logo/Logo";
 import BlogSidebar from "@components/BlogSidebar/BlogSidebar";
 import styles from "./BlogLayout.module.css";
@@ -9,48 +13,55 @@ import styles from "./BlogLayout.module.css";
 const BlogLayout = () => {
   return (
     <PageContainer>
-      <ColumnSection ratio="12rem:1" gapX="gx-16">
-        <InnerColumnSection
-          selector="nav"
-          direction="column"
-          className={`${styles.stickyPanel} ${styles.borderRight}`}
+      <PageBody
+        paddingTop={0}
+        paddingRight={16}
+        paddingBottom={0}
+        paddingLeft={16}
+      >
+        <ColumnSection
+          ratio="12rem:1"
+          gapX="gx-16"
+          className={styles.outerGrid}
         >
-          <Logo />
-          <ul>
-            <li>Kategorie</li>
-            <li>Tagi</li>
-            <li>Popularne</li>
-            <li>Najnowsze</li>
-            <li>Reklamy</li>
-          </ul>
-        </InnerColumnSection>
-        <InnerColumnSection
-          selector="section"
-          direction="column"
-          className={styles.section}
-        >
-          <div>Search</div>
-
-          <ColumnSection
-            ratio="1:15rem"
-            gapX="gx-16"
-            className={styles.contentGrid}
+          <InnerColumnSection
+            selector="nav"
+            direction="column"
+            className={`${styles.stickyPanel} ${styles.borderRight}`}
           >
-            <InnerColumnSection selector="main" direction="column">
-              <h2 id="blog-heading">Wpisy blogowe</h2>
-              <Outlet />
-            </InnerColumnSection>
-            <InnerColumnSection
-              selector="aside"
-              direction="column"
-              className={`${styles.stickyPanel} ${styles.borderLeft}`}
+            <Logo />
+            <ul>
+              <li>Kategorie</li>
+              <li>Tagi</li>
+              <li>Popularne</li>
+              <li>Najnowsze</li>
+              <li>Reklamy</li>
+            </ul>
+          </InnerColumnSection>
+          <InnerColumnSection selector="section" direction="column">
+            <div>Search</div>
+
+            <ColumnSection
+              ratio="1:15rem"
+              gapX="gx-16"
+              className={styles.contentGrid}
             >
-              <BlogSidebar />
-            </InnerColumnSection>
-          </ColumnSection>
-          <Footer />
-        </InnerColumnSection>
-      </ColumnSection>
+              <InnerColumnSection selector="main" direction="column">
+                <h2 id="blog-heading">Wpisy blogowe</h2>
+                <Outlet />
+              </InnerColumnSection>
+              <InnerColumnSection
+                selector="aside"
+                direction="column"
+                className={`${styles.stickyPanel} ${styles.borderLeft}`}
+              >
+                <BlogSidebar />
+              </InnerColumnSection>
+            </ColumnSection>
+            <Footer />
+          </InnerColumnSection>
+        </ColumnSection>
+      </PageBody>
     </PageContainer>
   );
 };
