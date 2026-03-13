d207e555c25ce7a7440dd63cf9d79a55ec046f64
Adam Zagórski <kontakt@adamzagorski.pl>
2026-03-13 09:25:57 +0100

    [#56] fix(css) fixed a few css settings to improve responsive display quality

--- my-monorepo/apps/web/src/leyouts/BlogLayout/BlogLayout.tsx
+++ my-monorepo/apps/web/src/leyouts/BlogLayout/BlogLayout.tsx
@@ -2,11 +2,7 @@ import { useCallback } from "react";
 import { Outlet } from "react-router";
 import { PageContainer } from "@packages/components/basePageContainers/PageContainer/PageContainer";
 import Footer from "@features/Footer/Footer";
-import {
-  ColumnSection,
-  InnerColumnSection,
-  PageBody,
-} from "@packages/components";
+import { ColumnSection, InnerColumnSection } from "@packages/components";
 import AsideData from "@components/AsideData/AsideData";
 import styles from "./BlogLayout.module.css";
 import SidebarMenuLayout from "@leyouts/SidebarMenuLayout/SidebarMenuLayout";
@@ -21,48 +17,37 @@ const BlogLayout = () => {
 
   return (
     <PageContainer>
-      <PageBody
-        paddingTop={0}
-        paddingRight={16}
-        paddingBottom={0}
-        paddingLeft={16}
-      >
-        <ColumnSection
-          ratio="12rem:1"
-          gapX="gx-16"
-          className={styles.outerGrid}
+      <ColumnSection ratio="12rem:1" gapX="gx-16" className={styles.outerGrid}>
+        <SidebarMenuLayout
+          selector="nav"
+          direction="column"
+          sidebarPosition="left"
         >
-          <SidebarMenuLayout
-            selector="nav"
-            direction="column"
-            sidebarPosition="left"
-          >
-            <SideTreeNavigation tree={blogFilterTree} onSearch={handleSearch} />
-          </SidebarMenuLayout>
-          <InnerColumnSection selector="section" direction="column">
-            <div>Search</div>
+          <SideTreeNavigation tree={blogFilterTree} onSearch={handleSearch} />
+        </SidebarMenuLayout>
+        <InnerColumnSection selector="section" direction="column">
+          <div>Search</div>
 
-            <ColumnSection
-              ratio="1:15rem"
-              gapX="gx-16"
-              className={styles.contentGrid}
+          <ColumnSection
+            ratio="1:15rem"
+            gapX="gx-16"
+            className={styles.contentGrid}
+          >
+            <InnerColumnSection selector="main" direction="column">
+              <h2 id="blog-heading">Wpisy blogowe</h2>
+              <Outlet />
+            </InnerColumnSection>
+            <SidebarMenuLayout
+              selector="aside"
+              direction="column"
+              sidebarPosition="right"
             >
-              <InnerColumnSection selector="main" direction="column">
-                <h2 id="blog-heading">Wpisy blogowe</h2>
-                <Outlet />
-              </InnerColumnSection>
-              <SidebarMenuLayout
-                selector="aside"
-                direction="column"
-                sidebarPosition="right"
-              >
-                <AsideData />
-              </SidebarMenuLayout>
-            </ColumnSection>
-            <Footer />
-          </InnerColumnSection>
-        </ColumnSection>
-      </PageBody>
+              <AsideData />
+            </SidebarMenuLayout>
+          </ColumnSection>
+          <Footer />
+        </InnerColumnSection>
+      </ColumnSection>
     </PageContainer>
   );
 };
